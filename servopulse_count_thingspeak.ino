342d351d4b2cd8821be56f5156240fab84578077
Biplov Paneru <90428280+Biplov01@users.noreply.github.com>
2022-12-21 18:55:30 +0545

    Add files via upload

--- servopulse_count_thingspeak.ino
+++ servopulse_count_thingspeak.ino
@@ -5,7 +5,7 @@ Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
 int pos = 0; 
-int pin = 7;
+int pin = 9;
 int duration;
 
 
@@ -30,8 +30,8 @@ int humidity;
 WiFiClient  client;
 void setup() {
   Serial.begin(9600);
-  pinMode(pin, INPUT);
-   myservo.attach(7);  // attaches the servo on pin 9 to the servo object
+  pinMode(pin, OUTPUT);
+   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
@@ -39,11 +39,11 @@ for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
 
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    delay(20);                       // waits 15ms for the servo to reach the position
   }
   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    delay(20);                       // waits 15ms for the servo to reach the position
   }
   duration = pulseIn(pin, HIGH);
   Serial.println(duration);
